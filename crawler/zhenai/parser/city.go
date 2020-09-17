c76b47745d41568670a9ba11eebd47a139336987
ZedLin <zed.lin@foxmail.com>
2020-09-17 17:28:50 +0800

    ParseCity : 解析城市页，提取用户详情页和用户名

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -7,19 +7,18 @@ import (
 	"github.com/zhibailin/go-distributed-crawler-from-scratch/crawler/engine"
 )
 
-const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
 
-func ParseCityList(contents []byte) engine.ParseResult {
-	re := regexp.MustCompile(cityListRe)
+func ParseCity(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityRe)
 	matches := re.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		// m[0] 是匹配的包含m[1]、m[2] 的整个字符串
-		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
+		fmt.Printf("User nick name: %s, URL: %s\n", m[2], m[1])
 		result.Items = append(result.Items, string(m[2]))
 		result.Requests = append(result.Requests, engine.Request{
 			Url:       string(m[1]),
-			ParseFunc: ParseCity,
+			ParseFunc: engine.NilParser,
 		})
 	}
 	return result
