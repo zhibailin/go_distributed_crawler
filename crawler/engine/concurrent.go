5153ad9bea639569ad4ca2b8ed674e0383c2d8d0
ZedLin <zed.lin@foxmail.com>
2020-09-18 17:55:53 +0800

    添加注释；Worker 里的 in 和 out 存在循环等待，卡死

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -21,26 +21,26 @@ func (e *ConcurrentEngine) Run(seeds ...Request) {
 	// 初始化管道配置
 	in := make(chan Request)
 	out := make(chan ParseResult)
-	e.Scheduler.ConfigureWorkerChan(in)
+	e.Scheduler.ConfigureWorkerChan(in) // 1. workChan = in，这里只有一个 channel
 
 	// engine 调用 NewWorker 并发创建 workers，
 	// 并发执行 fetch 和 parse
 	for i := 0; i < e.WorkerCount; i++ {
-		NewWorker(in, out)
+		NewWorker(in, out) // 2. 多个 worker 争抢一个workChan 里的 request；
 	}
 
 	// engine 令 Scheduler 向 管道 提交 requests
 	for _, r := range seeds {
-		e.Scheduler.Submit(r)
+		e.Scheduler.Submit(r) // 3. 向唯一的 workChan(in) 提交 request
 	}
 
 	for {
-		result := <-out
+		result := <-out // 4. 抢到request 的 worker 输出结果
 		for _, item := range result.Items {
 			log.Printf("Got item : %v", item)
 		}
 		for _, request := range result.Requests {
-			e.Scheduler.Submit(request)
+			e.Scheduler.Submit(request) // 5. 回到 2
 		}
 	}
 }
