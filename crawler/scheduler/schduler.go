2c24da271b7ce128f0032db710a09487a67973cc
ZedLin <zed.lin@foxmail.com>
2020-09-18 19:52:52 +0800

    实现队列调度器 QueueScheduler

--- crawler/scheduler/schduler.go
+++ crawler/scheduler/schduler.go
@@ -15,3 +15,49 @@ func (s *SimpleScheduler) Submit(request engine.Request) {
 		s.workerChan <- request
 	}()
 }
+
+type QueueScheduler struct {
+	requestChan chan engine.Request
+	workerChan  chan chan engine.Request
+}
+
+func (s *QueueScheduler) ConfigureWorkerChan(requests chan engine.Request) {
+	panic("implement me")
+}
+
+func (s *QueueScheduler) WorkerReady(w chan engine.Request) {
+	s.workerChan <- w
+}
+
+func (s *QueueScheduler) Submit(r engine.Request) {
+	s.requestChan <- r
+}
+
+func (s *QueueScheduler) Run() {
+	s.workerChan = make(chan chan engine.Request)
+	s.requestChan = make(chan engine.Request)
+
+	go func() {
+		var requestQ []engine.Request
+		var workerQ []chan engine.Request
+		for {
+			var activeRequest engine.Request
+			var activeWorker chan engine.Request
+
+			if len(requestQ) > 0 && len(workerQ) > 0 {
+				activeRequest = requestQ[0]
+				activeWorker = workerQ[0]
+			}
+			// 入队与出队
+			select {
+			case r := <-s.requestChan:
+				requestQ = append(requestQ, r)
+			case w := <-s.workerChan:
+				workerQ = append(workerQ, w)
+			case activeWorker <- activeRequest:
+				workerQ = workerQ[1:]
+				requestQ = requestQ[1:]
+			}
+		}
+	}()
+}
