97c4453aee466b5069409c83cbd32d80675140f6
ZedLin <zed.lin@foxmail.com>
2020-09-20 09:42:23 +0800

    重构 itemsaver.go : 使 elastic.NewClient  只执行一次；将 index 参数化

--- crawler/main.go
+++ crawler/main.go
@@ -8,10 +8,14 @@ import (
 )
 
 func main() {
+	itemChan, err := persist.ItemSaver("data_profile")
+	if err != nil {
+		panic(err)
+	}
 	e := engine.ConcurrentEngine{
 		Scheduler:   &scheduler.QueueScheduler{}, // 因为是指针接收者，所以加&
 		WorkerCount: 100,
-		ItemChan:    persist.ItemSaver(), // 注意这里有()，执行了 ItemSaver
+		ItemChan:    itemChan,
 	}
 	e.Run(engine.Request{
 		Url:       "http://www.zhenai.com/zhenghun/shanghai",
--- crawler/persist/itemsaver.go
+++ crawler/persist/itemsaver.go
@@ -10,7 +10,13 @@ import (
 	"gopkg.in/olivere/elastic.v5"
 )
 
-func ItemSaver() chan engine.Item {
+func ItemSaver(index string) (chan engine.Item, error) {
+	client, err := elastic.NewClient( // 默认到 9200 端口找服务器
+		elastic.SetSniff(false)) // Must turn off in docker；因为集群不在本机，而在docker里，image不通外网，无法sniff
+	if err != nil {
+		return nil, err
+	}
+
 	out := make(chan engine.Item)
 	go func() {
 		itemCount := 0
@@ -18,31 +24,26 @@ func ItemSaver() chan engine.Item {
 			item := <-out
 			log.Printf("Item Saver: got item "+"#%d: %v", itemCount, item)
 			itemCount++
-			err := Save(item)
+			err := Save(client, index, item)
 			if err != nil {
 				log.Printf("Item Saver: error "+"saving item %v: %v", item, err)
 			}
 		}
 	}()
-	return out // TODO: 返回出去的 out 和 go func() 里的 out 是什么关系
+	return out, nil
 }
 
-func Save(item engine.Item) error {
-	client, err := elastic.NewClient( // 默认到 9200 端口找服务器
-		elastic.SetSniff(false)) // Must turn off in docker；因为集群不在本机，而在docker里，image不通外网，无法sniff
-	if err != nil {
-		return err
-	}
+func Save(client *elastic.Client, index string, item engine.Item) error {
 	// 处理 Type 为空
 	if item.Type == "" {
 		return errors.New("must supply Type")
 	}
 	// 处理 Id 为空：先采用 Elasticsearch 自动填充的 Id(string)，若 item.Id 不为空再置换
-	indexService := client.Index().Index("data_profile").Type(item.Type).BodyJson(item)
+	indexService := client.Index().Index(index).Type(item.Type).BodyJson(item)
 	if item.Id != "" {
 		indexService.Id(item.Id)
 	}
-	_, err = indexService.Do(context.Background())
+	_, err := indexService.Do(context.Background())
 	if err != nil {
 		return err
 	}
