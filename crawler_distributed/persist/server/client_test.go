3012d1eaf1a817ef942674435f9f795693b5f897
ZedLin <zed.lin@foxmail.com>
2020-09-21 11:30:00 +0800

    测试 ItemSaver RPC Server

---
+++ crawler_distributed/persist/server/client_test.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/zhibailin/go-distributed-crawler-from-scratch/crawler_distributed/rpcsupport"
+
+	"github.com/zhibailin/go-distributed-crawler-from-scratch/crawler/engine"
+	"github.com/zhibailin/go-distributed-crawler-from-scratch/crawler/model"
+)
+
+func TestItemSaver(t *testing.T) {
+	const host = ":1234"
+
+	// Start ItemSaverServer
+	go func() {
+		_ = serveRpc(host, "dating_profile")
+	}()
+	time.Sleep(10 * time.Second)
+
+	// Start ItemSaverClient
+	client, err := rpcsupport.NewClient(host)
+	if err != nil {
+		panic(err)
+	}
+
+	// Call save
+	item := engine.Item{
+		Url:  "http://album.zhenai.com/u/108906739",
+		Type: "zhenai",
+		Id:   "108906739",
+		Payload: model.Profile{
+			Age:        34,
+			Height:     162,
+			Weight:     57,
+			Income:     "3001-5000元",
+			Gender:     "女",
+			Name:       "安静的雪",
+			Xinzuo:     "牧羊座",
+			Occupation: "人事/行政",
+			Marriage:   "离异",
+			House:      "已够房",
+			Hokou:      "山东菏泽",
+			Education:  "大学本科",
+			Car:        "未购车",
+		},
+	}
+	result := ""
+	err = client.Call("ItemSaverService.Save", item, &result)
+	if err != nil || result != "ok" {
+		t.Errorf("result: %s; err: %s", result, err)
+	}
+}
